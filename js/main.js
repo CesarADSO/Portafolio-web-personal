a599998aec3aed18d2b0204b52c006513c9a465f
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-10-04 13:54:09 -0500

    Realicé parte del responsive de smartphone. Todavía falta

--- js/main.js
+++ js/main.js
@@ -3,7 +3,36 @@ $('.habilidades-slider').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 1000,
-  arrows: false
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
 });
 
 $('.slider-proyectos').slick({
